f95a6599564553f5caeb82a1006fbf04f97aaff0
Mariusz Bijok <mbijok@outlook.com>
2023-09-05 11:27:56 +0200

    Test commit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Jan Kowalski 2';
+  const name = 'Jan Kowalski 3';
   greetUser(name);
 });
