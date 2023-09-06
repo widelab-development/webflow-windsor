bfd2fd27372f7626a37abbf19a8ab66cad861218
Mariusz Bijok <mbijok@outlook.com>
2023-09-06 16:25:34 +0200

    Added updated swiper

--- src/global-swipers.js
+++ src/global-swipers.js
@@ -1,3 +1,5 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs';
+
 const swiperBlogPosts = new Swiper('.swiper-blog-posts', {
   spaceBetween: 27,
   freeMode: false,
